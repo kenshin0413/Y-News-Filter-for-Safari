5ce772574f3c868571af41d7fc676ccd394b9b52
kenshin0413 <kenshin1234569@gmail.com>
2026-09-17 13:19:09 +0900

    初期

---
+++ Y!News Filter for Safari/Y!News Filter for Safari/Y_News_Filter_for_SafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  Y_News_Filter_for_SafariApp.swift
+//  Y!News Filter for Safari
+//
+//  Created by miyamotokenshin on R 8/09/17.
+//
+
+import SwiftUI
+
+@main
+struct Y_News_Filter_for_SafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
